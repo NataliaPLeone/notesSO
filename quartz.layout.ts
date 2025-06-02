52235fa990492118d7abd1d663e3b2d9c4c46545
Natalia PL <np2020220@gmail.com>
2025-06-02 02:25:37 -0300

    Quartz sync: Jun 2, 2025, 2:25 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,12 +5,15 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.RecentNotes({ title: "Páginas adicionadas recentemente" ,showTags: false,limit: 7})
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
+    
   }),
 }
 
